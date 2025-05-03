18be6162d053006e6b702c72228bf8690010aeaf
84176052+sounddrill31 <84176052+sounddrill31@users.noreply.github.com>
2025-05-03 23:04:08 +0530

    Switch to tabs

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,16 +1,48 @@
-import { Stack } from "expo-router";
+import { Tabs } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View, Text, Image } from "react-native";
+import React from "react";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
 const RootLayout = () => {
   return (
-    <Stack
-      screenOptions={{
-        headerStyle: { backgroundColor: "#ddd" },
-        headerTintColor: "#333",
-      }}
-    >
-      <Stack.Screen name="index" options={{ title: "HomePage" }} />
-      <Stack.Screen name="about" options={{ title: "About App" }} />
-    </Stack>
+    <React.Fragment>
+      <StatusBar style="auto" />
+      <Tabs
+        screenOptions={{
+          headerStyle: { backgroundColor: "#ddd" },
+          headerTintColor: "#333",
+          tabBarActiveTintColor: "teal",
+        }}
+      >
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: "HomePage",
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons
+                name="numeric-1-box-outline"
+                size={size}
+                color={color}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="about"
+          options={{
+            title: "About App",
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons
+                name="numeric-2-box-outline"
+                size={size}
+                color={color}
+              />
+            ),
+          }}
+        />
+      </Tabs>
+    </React.Fragment>
   );
 };
 
